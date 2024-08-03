1ba4a1822b23b906b62c733ed7a483280752957f
Rohan-IIT <145170883+Rohan-IIT@users.noreply.github.com>
2024-08-03 17:05:41 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,6 @@
+import mymodule
+
+if __name__ == "__main__":
+    print("This script is being run directly.")
+else:
+    print("This script is being imported as a module.")
---
+++ mymodule.py
@@ -0,0 +1,7 @@
+import main
+
+def helper_function():
+    print("This is a helper function.")
+
+def another_helper_function():
+    print("This is another helper function.")
